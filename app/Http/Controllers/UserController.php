8a74170330025214303367d3dd21ffbd22f8c155
RONLEY VINBERK <ronleyvinberk@gmail.com>
2018-02-14 17:31:40 +0700

    Route Model Data Binding

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -82,7 +82,7 @@ class UserController extends Controller
             return redirect()->back()->withErrors($validator)->withInput();
         }
 
-		$dataUpdate['password_confirmation'] = Hash::make($dataUpdate['password_confirmations']);
+		$dataUpdate['password_confirmation'] = Hash::make($dataUpdate['password_confirmation']);
 	    $dataUpdate['usertype'] = 2;
 
 		if (!$user->update($dataUpdate)) {
